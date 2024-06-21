7bad1ad68771f2106c567d6c49ee153af07bfc2a
Anders Eliasson <anders.eliasson@digitalroute.com>
2024-06-21 11:59:26 +0200

    more impl

--- src/btcbalance/core.clj
+++ src/btcbalance/core.clj
@@ -1,12 +1,17 @@
 (ns btcbalance.core
   (:use [clojure.pprint])
-  (:require [bchain.core :refer [dbg rawaddr SEK SEK-last USD USD-last satoshi blocks historical-price]]
-            [bchain.shape :as ss]
-            [bchain.balance :refer [convert balance def-balance total-balance convert-to store! all-balance store-proc]]
-            [clj-ldap.client :as ldap]
-            )
+  #_(:require [bchain.core :refer [dbg rawaddr SEK SEK-last USD USD-last satoshi blocks historical-price]]
+             [bchain.shape :as ss]
+             [bchain.balance :refer [convert balance def-balance total-balance convert-to store! all-balance store-proc]]
+            
+             )
   (:import [java.net URL]
            [java.text SimpleDateFormat]))
+
+(defmacro dbg [body]
+  `(let [x# ~body]
+     (println "dbg:" '~body "=" x#)
+     x#))
 
 (defn url-get [url]
   (let [con (.openConnection (URL. url))
@@ -119,11 +124,59 @@
 (defn buy-btc? [] (< (mayer-multiple-of 0) (average-multiple)))
   
 
+(defn btc-powerlaw [year] 
+  (Math/pow (/ (+ year 1) year) 6))
+
+(defn btc-growth-fn [value start-year]
+  (* value (btc-powerlaw start-year)))
+
+(defn btc-growth [value start-year years]
+  (reduce 
+    (fn [acc v] (btc-growth-fn acc v)) value (range start-year (+ start-year years)))
+  )
+
+(defn giovani-price [days] (* (Math/pow 10 -17) (Math/pow days 5.83)))
+
+(defn growth-fn [factor acc v]
+  (let [[v y] (first acc)]
+    (cons [(* factor v) (inc y)] acc))) 
+  
+
+(defn growth [value percent years]
+  (let [factor (+ 1 (/ percent 100))]
+    (reduce (partial growth-fn factor) [[value 0]] (range years)))) 
+  
+(defn passive-net-income [fortune percent]
+  (* 0.7 (/ (- (growth fortune percent 1) fortune) 12)))
+
+
+(def btc-usd-price [[0.11 2010][20.96 2011][10.89 2012]
+                       [101.95 2013][592 2014][244 2015]
+                       [764 2016][2942 2017][6164 2018]
+                       [8036 2019][9480 2020][35850 2021]
+                       [20473 2022][26505 2023][65000 2024]])
+(def btc-usd-year-map (apply merge (map (fn [[v y]] {y v}) btc-usd-price))) 
+
+(def usd-sek [[7.84 2010][6.4 2011][6.99 2012]
+              [2.56 2013][6.63 2014][8.29 2015]
+              [8.33 2016][8.71 2017][8.71 2018]
+              [9.54 2019][9.32 2020][8.26 2021]
+              [9.54 2022][10.8 2023][10.52 2024]])
+
+(def usd-sek-year-map (apply merge (map (fn [[v y]] {y v}) usd-sek))) 
+
+(def btc-sek-price-map (apply merge (map (fn [[v y]] {y (* v (get btc-usd-year-map y))}) usd-sek)))
+
+(def kardborrevagen-btc-price
+  (let [v (reverse (map (fn [[v y]] [v (+ 2003 y)]) (growth 2.5e6 5.5 21)))]
+    (map 
+      (fn [[v y]]
+        (if-let [x (dbg (get btc-sek-price-map y))]
+          [(/ v x) y]
+          [nil y])) 
+        v)
+    ))
 
 
 (String/format "You should %sbuy btc!" (into-array String [(if (buy-btc?) "" "not ")]))
-
-
-
-
 
