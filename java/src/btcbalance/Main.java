83b9aa475dd2eceb4f9c72c382188eecf805e30d
Anders Eliasson <anders.eliasson@digitalroute.com>
2025-04-15 20:58:50 +0200

    stuff

--- java/src/btcbalance/Main.java
+++ java/src/btcbalance/Main.java
@@ -6,15 +6,21 @@ import clojure.lang.IFn;
 public class Main {
 	private static final String CLOJURE_CORE = "clojure.core";
 	private static final String BTCBALANCE_CORE = "btcbalance.core";
-	private static final String BTCBALANCE_ANDERS = "btcbalance.secure.anders";
+	private static final String BTCBALANCE_ASSETS = "btcbalance.secure.assets";
 
 	public static void main(final String[] args) {
 		final IFn require = Clojure.var(CLOJURE_CORE, "require");
-		require.invoke(Clojure.read(BTCBALANCE_CORE));
-		require.invoke(Clojure.read(BTCBALANCE_ANDERS));
-		final IFn save = Clojure.var(BTCBALANCE_CORE, "save!");
-		save.invoke();
-		final IFn saveRates = Clojure.var(BTCBALANCE_ANDERS, "save-rates!");
+		
+		try {
+			require.invoke(Clojure.read(BTCBALANCE_CORE));
+			final IFn save = Clojure.var(BTCBALANCE_CORE, "save!");
+			save.invoke();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		require.invoke(Clojure.read(BTCBALANCE_ASSETS));
+		final IFn saveRates = Clojure.var(BTCBALANCE_ASSETS, "save-rates!");
 		saveRates.invoke();
 
 	}
