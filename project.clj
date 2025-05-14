019257a7e9117589466112e3b7a48449a21ea9d7
Anders Eliasson <anders.eliasson@digitalroute.com>
2025-05-14 16:45:17 +0200

    stuff

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot [btcbalance.core]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [bchain "0.1.0-SNAPSHOT"]
                  [clj-twilio "0.2.0"]
                  [org.clojure/data.json "0.2.4"]
